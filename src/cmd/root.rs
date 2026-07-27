5c3e7768f2dbf1e0082980fa7de8ade06adfac04
Fovir <fovir@disroot.org>
2026-07-28 04:36:25 +0800

    feat(cmd): implement `injm` root command
    Signed-off-by: Fovir <fovir@disroot.org>

--- src/cmd/root.rs
+++ src/cmd/root.rs
@@ -1,5 +1,43 @@
-use crate::cli::GlobalArgs;
+use std::fs;
 
-pub fn run(_args: GlobalArgs) -> anyhow::Result<()> {
-    todo!()
+use crate::{
+    cli::{GlobalArgs, InjectArgs},
+    cmd::inject,
+    config::load_config,
+};
+use anyhow::{Result, anyhow};
+
+pub fn run(mut args: GlobalArgs) -> Result<()> {
+    let config_path = match &args.config {
+        Some(cfg) => Some(cfg.to_owned()),
+        None => {
+            let cwd = std::env::current_dir()?;
+            let path = cwd.join("injm.toml");
+            if fs::exists(&path)? {
+                Some(path)
+            } else {
+                return Err(anyhow!("config file is missing"));
+            }
+        }
+    };
+
+    args.config = config_path.clone();
+    let cfg = load_config(config_path)?;
+
+    if cfg.input.is_empty() {
+        return Err(anyhow!("input source is empty"));
+    }
+
+    inject::run(
+        InjectArgs {
+            input: cfg.input,
+            output: cfg.output,
+            dry_run: false,
+            diff: false,
+            id: vec![],
+        },
+        args,
+    )?;
+
+    Ok(())
 }
