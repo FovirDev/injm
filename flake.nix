1b5632c3f330008e72b298f5595ce3556b7d31c6
Fovir <fovir@disroot.org>
2026-08-13 16:22:48 +0800

    chore: release v1.0.0
    Signed-off-by: Fovir <fovir@disroot.org>

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "injm";
-          version = "0.8.0";
+          version = "1.0.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
           preBuild = ''
